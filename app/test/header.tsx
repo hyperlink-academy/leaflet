9b7b0ec045081993a0dab26852cffadf5d24248c
celine <celine@hyperlink.academy>
2024-05-16 16:02:22 -0400

    added add and remove card buttons for testing purposes
    the card carousel now maps over an array and can add or remove cards to itself with buttons on the card. This will set me up for scrolling the right card into view

---
+++ app/test/header.tsx
@@ -0,0 +1,17 @@
+import { Home } from "../../components/Icons";
+
+export const PageHeader = () => {
+  return (
+    <div className="pageHeader shrink-0 flex justify-between grow-0 mx-4">
+      <div className="w-[3px] bg-test h-6 mx-auto" />
+      {/* <div className="flex gap-4 items-center">
+      <div className="text-grey-55">
+        <Home />
+      </div>
+
+      <ButtonPrimary>Share!</ButtonPrimary>
+    </div>
+    <div>theme</div> */}
+    </div>
+  );
+};
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,66 +1,58 @@
 "use client";
 
-import { Home } from "../../components/Icons";
 import { ButtonPrimary } from "../../components/Buttons";
 import { useState } from "react";
 import useMeasure from "react-use-measure";
+import { PageHeader } from "./header";
 
 export default function Index() {
   let [cardRef, { width: cardWidth }] = useMeasure();
-  let [spacerRef, { width: spacerWidth }] = useMeasure();
-
-  let [cardTwo, setCardTwo] = useState(false);
-  let [cardMany, setCardMany] = useState(false);
+  let [cards, setCards] = useState([0]);
 
   return (
     <div className="pageWrapper h-screen flex flex-col gap-4 py-4">
-      <div className="pageHeader shrink-0 flex justify-between grow-0 mx-4">
-        <div className="w-[3px] bg-test h-6 mx-auto" />
-        {/* <div className="flex gap-4 items-center">
-          <div className="text-grey-55">
-            <Home />
-          </div>
-
-          <ButtonPrimary>Share!</ButtonPrimary>
-        </div>
-        <div>theme</div> */}
-      </div>
+      <PageHeader />
 
       <div className="pageContentWrapper w-full overflow-x-scroll snap-mandatory snap-x grow items-stretch flex ">
-        <div className="pageContent flex mx-auto">
+        <div className="pageContent flex">
           <div style={{ width: `calc((100vw - ${cardWidth}px)/2)` }} />
-          <div className="h-full flex items-stretch" ref={cardRef}>
-            <Card first>
-              <ButtonPrimary
-                onClick={() => {
-                  setCardTwo(!cardTwo);
-                }}
-              >
-                toggle card 2
-              </ButtonPrimary>
-              <ButtonPrimary
-                onClick={() => {
-                  setCardMany(!cardMany);
-                }}
-              >
-                toggle card many
-              </ButtonPrimary>
-            </Card>
-          </div>
-          {cardTwo && <Card>Card 2</Card>}
-          {cardMany && (
-            <>
-              <Card>Card 2</Card>
-              <Card>Card 3</Card>
-              <Card>Card 4</Card>
-              <Card>Card 5</Card>
-              <Card>Card 6</Card>
-              <Card>Card 7</Card>
-            </>
-          )}
+
+          {cards.map((card, index) => (
+            <div className="flex items-stretch" ref={cardRef}>
+              <Card first={index === 0} key={index} id={index.toString()}>
+                Card {card}
+                <ButtonPrimary
+                  onClick={() => {
+                    //add a new card after this one
+                    setCards([...cards, card + 1]);
+
+                    //scroll the new card into view
+                    document
+                      .getElementById((index + 1).toString())
+                      ?.scrollIntoView();
+                  }}
+                >
+                  add card
+                </ButtonPrimary>
+                {index !== 0 && (
+                  <ButtonPrimary
+                    onClick={() => {
+                      cards.splice(index, 1);
+                      setCards([...cards]);
+                    }}
+                  >
+                    remove card
+                  </ButtonPrimary>
+                )}
+                {/* <ButtonPrimary onClick={() => {}}>
+                  focus this card
+                </ButtonPrimary> */}
+              </Card>
+            </div>
+          ))}
+
           <div
-            ref={spacerRef}
-            style={{ width: `calc((100vw / 2) - ${cardWidth}px)` }}
+            style={{ width: `calc((100vw / 2) - ${cardWidth}px + 12px )` }}
           />
         </div>
       </div>
@@ -68,13 +60,18 @@ export default function Index() {
   );
 }
 
-const Card = (props: { children: React.ReactNode; first?: boolean }) => {
+const Card = (props: {
+  children: React.ReactNode;
+  first?: boolean;
+  id: string;
+}) => {
   return (
     <>
       {/* if the card is the first one in the list, remove this div... can we do with :before? */}
-      {!props.first && <div className="w-8 snap-center" />}
+      {!props.first && <div className="w-6 snap-center" />}
       <div
-        className={`p-3 w-[calc(50vw-24px)] max-w-[500px] bg-bg-card border border-grey-80 rounded-lg grow flex flex-col gap-2 ${props.first && "snap-center"}`}
+        id={props.id}
+        className={`p-3 w-[calc(50vw-24px)] max-w-prose bg-bg-card border border-grey-80 rounded-lg grow flex flex-col gap-2 ${props.first && "snap-center"}`}
       >
         {props.children}
       </div>
