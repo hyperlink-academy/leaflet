2ce802a83f530992ef1ded6f58f59c457a4c261f
Jared Pereira <jared@awarm.space>
2024-05-21 16:25:25 -0400

    add /new route

---
+++ app/new/page.tsx
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import { entities } from "../../drizzle/schema";
+import { redirect } from "next/navigation";
+import postgres from "postgres";
+const client = postgres(process.env.DB_URL as string);
+const db = drizzle(client);
+
+export default async function RootPage() {
+  let rows = await db.insert(entities).values({}).returning();
+  return redirect(`/${rows[0].id}`);
+}
