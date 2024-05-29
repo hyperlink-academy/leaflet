dfad5209fae6910b17ac82f593a5e4b4c0c3f700
Jared Pereira <jared@awarm.space>
2024-05-30 00:15:27 +0100

    log service worker registration

--- components/ServiceWorker.tsx
+++ components/ServiceWorker.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 export function ServiceWorker() {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/worker.js");
+      navigator.serviceWorker.register("/worker.js").then(console.log);
     }
   }, []);
   return null;
