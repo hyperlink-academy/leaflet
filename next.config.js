9f1c1ba3bd2ba07b0ad822a32d07b7ea7e17b2a0
Jared Pereira <jared@awarm.space>
2024-05-29 20:03:07 +0100

    lingering react aria stuff

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
       { protocol: "http", hostname: "127.0.0.1", port: "54321" },
     ],
   },
-  transpilePackages: ["@adobe/react-spectrum", "@react-spectrum/color"],
   experimental: {
     instrumentationHook: true,
   },
