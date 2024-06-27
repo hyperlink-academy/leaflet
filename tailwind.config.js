44de3563aa5c25b45125ea7b8b94c6e7ce8b207e
celine <celine@hyperlink.academy>
2024-06-27 19:24:40 -0400

    changed our color-mix profile to produce more intuitive interpolated colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,28 +18,28 @@ module.exports = {
       current: "currentColor",
 
       //TEXT COLORS.
-      "primary" : "rgb(var(--primary))",
-      "secondary": "color-mix(in lch, rgb(var(--primary)), rgb(var(--bg-card)) 15%)",
-      "tertiary": "color-mix(in lch, rgb(var(--primary)), rgb(var(--bg-card)) 55%)",
-      "border": "color-mix(in lch, rgb(var(--primary)), rgb(var(--bg-card)) 75%)",
-      "border-light": "color-mix(in lch, rgb(var(--primary)), rgb(var(--bg-card)) 85%)",
+      primary: "rgb(var(--primary))",
+      secondary: "color-mix(in oklab, rgb(var(--primary)), white 15%)",
+      tertiary:
+        "color-mix(in oklab, rgb(var(--primary)), rgb(var(--bg-card)) 55%)",
+      border:
+        "color-mix(in oklab, rgb(var(--primary)), rgb(var(--bg-card)) 75%)",
+      "border-light":
+        "color-mix(in oklab, rgb(var(--primary)), rgb(var(--bg-card)) 85%)",
 
       white: "#FFFFFF",
 
       //ACCENT COLORS
-      "accent": "rgb(var(--accent))",
-      "accentText": "rgb(var(--accent-text))",
-
+      accent: "rgb(var(--accent))",
+      accentText: "rgb(var(--accent-text))",
 
       //BG COLORS (defined as css variables in global.css)
       "bg-page": "rgb(var(--bg-page))",
       "bg-card": "rgb(var(--bg-card))",
 
       //DO NOT USE IN PRODUCTION. Test colors to aid development, ie, setting bg color on element to see edges of div. DO. NOT. USE. IN. PRODUCTION
-      "test": "#E18181",
+      test: "#E18181",
       "test-blue": "#48D1EF",
-
-
     },
     fontSize: {
       xs: ".75rem",
@@ -52,17 +52,12 @@ module.exports = {
 
     extend: {
       boxShadow: {
-
-       sm:"0.9px 1.5px 1.7px -1.8px rgba(var(--primary), 0.2), 4.2px 6.9px 7.8px -3.5px rgba(var(--primary), 0.15);",
-       md:"1.2px 2.5px 2.7px -1.8px rgba(var(--primary), 0.1), 5.6px 11.6px 12.5px -3.5px rgba(var(--primary), 0.15);"
-
-
-
-
+        sm: "0.9px 1.5px 1.7px -1.8px rgba(var(--primary), 0.2), 4.2px 6.9px 7.8px -3.5px rgba(var(--primary), 0.15);",
+        md: "1.2px 2.5px 2.7px -1.8px rgba(var(--primary), 0.1), 5.6px 11.6px 12.5px -3.5px rgba(var(--primary), 0.15);",
       },
 
       fontFamily: {
-        sans: ['var(--font-quattro)'],
+        sans: ["var(--font-quattro)"],
       },
     },
   },
