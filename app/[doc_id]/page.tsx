f55cab3b8f82ec432a9329dbb031a722567634a6
celine <celine@hyperlink.academy>
2024-07-03 18:10:35 -0400

    bare minimum 404

--- app/[doc_id]/page.tsx
+++ app/[doc_id]/page.tsx
@@ -35,7 +35,22 @@ export default async function DocumentPage(props: Props) {
     .single();
   let rootEntity = res.data?.root_entity;
   if (!rootEntity || !res.data)
-    return <div>404 no rootEntity found idk man</div>;
+    return (
+      <div className="w-screen h-screen flex place-items-center bg-bg-page">
+        <div className="bg-bg-card mx-auto p-4 border border-border rounded-md flex flex-col text-center justify-centergap-1 w-fit">
+          <div className="font-bold">
+            Hmmm... Couldn&apos;t find that leaflet.
+          </div>
+          <div>
+            You can{" "}
+            <a href="mailto:contact@hyperlink.academy" target="blank">
+              email us
+            </a>{" "}
+            for help!
+          </div>
+        </div>
+      </div>
+    );
   let { data } = await supabase.rpc("get_facts", {
     root: rootEntity,
   });
