ffabfe7d3461daff61f3e90f5e797eb66deee34b
Jared Pereira <jared@awarm.space>
2024-07-07 02:30:39 -0400

    set size and get response in og-image generation

--- app/[doc_id]/opengraph-image.tsx
+++ app/[doc_id]/opengraph-image.tsx
@@ -1,5 +1,5 @@
-import { get_url_preview_data } from "actions/addLinkCard";
 import { headers } from "next/headers";
+import { ImageResponse } from "next/og";
 export const runtime = "edge";
 export default async function OpenGraphImage(props: {
   params: { doc_id: string };
@@ -9,12 +9,17 @@ export default async function OpenGraphImage(props: {
   const hostname = headersList.get("x-forwarded-host");
   let protocol = headersList.get("x-forwarded-proto");
   let path = `${protocol}://${hostname}/${props.params.doc_id}`;
-  return fetch(
-    `https://pro.microlink.io/?url=${path}&screenshot=true&meta=false&embed=screenshot.url`,
+  let startTime = Date.now();
+  console.log("starting fetch");
+  let response = await fetch(
+    `https://pro.microlink.io/?url=${path}&screenshot=true&&viewport.width=1200&viewport.height=630&meta=false&embed=screenshot.url`,
     {
       headers: {
         "x-api-key": process.env.MICROLINK_API_KEY!,
       },
     },
   );
+  let endTime = Date.now();
+  console.log(`fetched in ${(endTime - startTime) / 1000} seconds`);
+  return response;
 }
