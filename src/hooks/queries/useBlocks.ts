0e797c4f623ca404d1456719453703b3add14af0
Jared Pereira <jared@awarm.space>
2024-07-17 14:12:50 -0400

    fix initial blocks query

--- src/hooks/queries/useBlocks.ts
+++ src/hooks/queries/useBlocks.ts
@@ -88,44 +88,49 @@ export const getBlocksWithTypeLocal = (
 ) => {
   let scan = scanIndexLocal(initialFacts);
   let blocks = scan.eav(entityID, "card/block");
-
-  return blocks.flatMap((b) => {
-    let type = scan.eav(b.data.value, "block/type")[0];
-    let isList = scan.eav(b.data.value, "block/is-list");
-    if (!type) return null;
-    if (isList[0]?.data.value) {
-      const getChildren = (
-        root: Fact<"card/block">,
-        parent: string,
-        depth: number,
-        path: { depth: number; entity: string }[],
-      ): Block[] => {
-        let children = scan.eav(root.data.value, "card/block");
-        let type = scan.eav(b.data.value, "block/type")[0];
-        let newPath = [...path, { entity: b.entity, depth }];
-        let childBlocks = children.map((c) =>
-          getChildren(c, root.data.value, depth + 1, newPath),
-        );
-        return [
-          {
-            ...root.data,
-            factID: root.id,
-            type: type.data.value,
-            parent: b.entity,
-            listData: { depth: depth, parent, path: newPath },
-          },
-          ...childBlocks.flat(),
-        ];
-      };
-      return getChildren(b, b.entity, 1, []);
-    }
-    return [
-      {
-        ...b.data,
-        factID: b.id,
-        type: type.data.value,
-        parent: b.entity,
-      },
-    ] as Block[];
-  });
+  return blocks
+    .sort((a, b) => (a.data.position > b.data.position ? 1 : -1))
+    .map((b) => {
+      let type = scan.eav(b.data.value, "block/type")[0];
+      let isList = scan.eav(b.data.value, "block/is-list");
+      if (!type) return null;
+      if (isList[0]?.data.value) {
+        const getChildren = (
+          root: Fact<"card/block">,
+          parent: string,
+          depth: number,
+          path: { depth: number; entity: string }[],
+        ): Block[] => {
+          let children = scan
+            .eav(root.data.value, "card/block")
+            .sort((a, b) => (a.data.position > b.data.position ? 1 : -1));
+          let type = scan.eav(root.data.value, "block/type")[0];
+          let newPath = [...path, { entity: root.data.value, depth }];
+          let childBlocks = children.map((c) =>
+            getChildren(c, root.data.value, depth + 1, newPath),
+          );
+          return [
+            {
+              ...root.data,
+              factID: root.id,
+              type: type.data.value,
+              parent: b.entity,
+              listData: { depth: depth, parent, path: newPath },
+            },
+            ...childBlocks.flat(),
+          ];
+        };
+        return getChildren(b, b.entity, 1, []);
+      }
+      return [
+        {
+          ...b.data,
+          factID: b.id,
+          type: type.data.value,
+          parent: b.entity,
+        },
+      ] as Block[];
+    })
+    .flat()
+    .filter((f) => f !== null);
 };
