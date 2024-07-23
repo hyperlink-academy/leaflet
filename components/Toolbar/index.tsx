55e35b9fc91732f53937c9bfb78395f33f51dafd
Jared Pereira <jared@awarm.space>
2024-07-23 18:14:16 -0400

    remove stray char

--- components/Toolbar/index.tsx
+++ components/Toolbar/index.tsx
@@ -146,7 +146,6 @@ export const TextToolbar = (props: { cardID: string; blockID: string }) => {
                         Highlight
                       </div>
                       <div className="flex gap-1">
-                        $
                         {isMac() ? (
                           <>
                             <ShortcutKey>⌘</ShortcutKey> +{" "}
