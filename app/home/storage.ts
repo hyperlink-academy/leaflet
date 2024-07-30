1ab3511247c4243bc31d1946c95fc8465bc01d9c
Jared Pereira <jared@awarm.space>
2024-07-30 02:15:18 -0400

    version home storage

--- app/home/storage.ts
+++ app/home/storage.ts
@@ -1,16 +1,29 @@
 import { PermissionToken } from "src/replicache";
 
 export type HomeDoc = { token: PermissionToken; added_at: string };
+type HomeDocsStorage = {
+  version: number;
+  docs: Array<HomeDoc>;
+};
+let defaultValue: HomeDocsStorage = {
+  version: 1,
+  docs: [],
+};
+const key = "homepageDocs-v1";
 export function getHomeDocs() {
-  let homepageDocs: Array<HomeDoc> = JSON.parse(
-    window.localStorage.getItem("homepageDocs") || "[]",
+  let homepageDocs: HomeDocsStorage = JSON.parse(
+    window.localStorage.getItem(key) || JSON.stringify(defaultValue),
   );
-  return homepageDocs;
+  return homepageDocs.docs;
 }
 
 export function addDocToHome(doc: PermissionToken) {
   let homepageDocs = getHomeDocs();
   if (homepageDocs.find((d) => d.token.id === doc.id)) return;
   homepageDocs.push({ token: doc, added_at: new Date().toISOString() });
-  window.localStorage.setItem("homepageDocs", JSON.stringify(homepageDocs));
+  let newValue: HomeDocsStorage = {
+    version: 1,
+    docs: homepageDocs,
+  };
+  window.localStorage.setItem(key, JSON.stringify(newValue));
 }
