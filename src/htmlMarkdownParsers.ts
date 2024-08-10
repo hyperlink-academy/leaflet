4b12bcb79af08929df0eaf306525a65b04fbe83a
Jared Pereira <jared@awarm.space>
2024-08-09 20:01:48 -0400

    make copied markdown lists tight always

--- src/htmlMarkdownParsers.ts
+++ src/htmlMarkdownParsers.ts
@@ -3,9 +3,23 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 
+import { visit } from "unist-util-visit";
 import rehypeParse from "rehype-parse";
 import rehypeRemark from "rehype-remark";
 import remarkStringify from "remark-stringify";
+
+function remarkTightListify() {
+  return (tree: any) => {
+    visit(tree, "list", (node) => {
+      node.spread = false;
+      for (const child of node.children) {
+        if (child.type === "listItem") {
+          child.spread = false;
+        }
+      }
+    });
+  };
+}
 
 export function markdownToHtml(markdown: string): string {
   return String(
@@ -13,7 +27,7 @@ export function markdownToHtml(markdown: string): string {
       .use(remarkParse) // Parse markdown content to a syntax tree
       .use(remarkRehype) // Turn markdown syntax tree to HTML syntax tree, ignoring embedded HTML
       .use(rehypeStringify) // Serialize HTML syntax tree
-      .processSync(markdown)
+      .processSync(markdown),
   );
 }
 
@@ -22,12 +36,10 @@ export function htmlToMarkdown(html: string): string {
     unified()
       .use(rehypeParse) // Parse HTML to a syntax tree
       .use(rehypeRemark) // Turn HTML syntax tree to markdown syntax tree
-      .use(
-        remarkStringify, // Serialize HTML syntax tree
-        {
-          bullet: "-", // change default list marker from '*'
-        }
-      )
-      .processSync(html)
+      .use(remarkTightListify)
+      .use(remarkStringify, {
+        bullet: "-", // change default list marker from '*'
+      }) // Serialize HTML syntax tree
+      .processSync(html),
   );
 }
