c30d5336d11badc98810b1b587dac6e7d91a6c2f
Jared Pereira <jared@awarm.space>
2024-08-27 13:57:57 -0400

    cancel longpress on mouse move

--- src/hooks/useLongPress.ts
+++ src/hooks/useLongPress.ts
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 export const useLongPress = (
   cb: () => void,
@@ -7,17 +7,34 @@ export const useLongPress = (
 ) => {
   let longPressTimer = useRef<number>();
   let isLongPress = useRef(false);
+  let [isDown, setIsdown] = useState(false);
 
   let start = (e: React.MouseEvent) => {
     onMouseDown && onMouseDown(e);
+    setIsdown(true);
     isLongPress.current = false;
     longPressTimer.current = window.setTimeout(() => {
       isLongPress.current = true;
       cb();
     }, 500);
   };
+
+  useEffect(()=> {
+    if (isDown) {
+      let listener = (e: MouseEvent) => {
+        end() 
+      }
+      window.addEventListener("mousemove", listener);
+      return ()=> {
+        window.removeEventListener("mousemove", listener);
+      };
+    }
+  }, [isDown]);
+
   let end = () => {
+    setIsdown(false);
     window.clearTimeout(longPressTimer.current);
+    longPressTimer.current = undefined;
   };
   let click = (e: React.MouseEvent | React.PointerEvent) => {
     if (isLongPress.current) e.preventDefault();
