df834cc5adbce05984b2b4ec29fcec015c0e0dfc
Jared Pereira <jared@awarm.space>
2024-09-18 22:00:39 -0400

    fix some selection wierdness

--- components/SelectionManager.tsx
+++ components/SelectionManager.tsx
@@ -32,14 +32,19 @@ export function SelectionManager() {
     const getSortedSelection = async () => {
       let selectedBlocks = useUIState.getState().selectedBlocks;
       let foldedBlocks = useUIState.getState().foldedBlocks;
+      if (!selectedBlocks[0]) return [];
       let siblings =
         (await rep?.query((tx) =>
           getBlocksWithType(tx, selectedBlocks[0].parent),
         )) || [];
       let sortedBlocks = siblings.filter((s) => {
         let selected = selectedBlocks.find((sb) => sb.value === s.value);
+        return selected;
+      });
+      let sortedBlocksWithChildren = siblings.filter((s) => {
+        let selected = selectedBlocks.find((sb) => sb.value === s.value);
         if (s.listData && !selected) {
-          //Select the children of folded list blocks
+          //Select the children of folded list blocks (in order to copy them)
           return s.listData.path.find(
             (p) =>
               selectedBlocks.find((sb) => sb.value === p.entity) &&
@@ -48,7 +53,17 @@ export function SelectionManager() {
         }
         return selected;
       });
-      return [sortedBlocks, siblings];
+      return [
+        sortedBlocks,
+        siblings.filter(
+          (f) =>
+            !f.listData ||
+            !f.listData.path.find(
+              (p) => foldedBlocks.includes(p.entity) && p.entity !== f.value,
+            ),
+        ),
+        sortedBlocksWithChildren,
+      ];
     };
     let removeListener = addShortcut([
       {
@@ -410,8 +425,6 @@ export function SelectionManager() {
           let focusedBlockIndex = sortedSelection.findIndex(
             (s) => s.value == b.entityID,
           );
-          let siblings =
-            (await rep?.query((tx) => getBlocksWithType(tx, b.parent))) || [];
           if (focusedBlockIndex === sortedSelection.length - 1) {
             let index = siblings.findIndex((s) => s.value === b.entityID);
             let nextSelectedBlock = siblings[index + 1];
@@ -457,8 +470,8 @@ export function SelectionManager() {
       }
       if (e.key === "c" && (e.metaKey || e.ctrlKey)) {
         if (!rep) return;
-        let [sortedSelection] = await getSortedSelection();
-        await copySelection(rep, sortedSelection);
+        let [, , selectionWithFoldedChildren] = await getSortedSelection();
+        await copySelection(rep, selectionWithFoldedChildren);
       }
     };
     window.addEventListener("keydown", listener);
