8f82c98d5cb72c6af65d47cb9356502086dc4836
Jared Pereira <jared@awarm.space>
2024-11-08 15:46:07 -0500

    add some styling

--- components/Blocks/DateTimeBlock.tsx
+++ components/Blocks/DateTimeBlock.tsx
@@ -3,26 +3,58 @@ import { BlockProps } from "./Block";
 import { DayPicker } from "react-day-picker";
 import * as Popover from "@radix-ui/react-popover";
 import { useMemo, useState } from "react";
+import { BlockCalendarSmall } from "components/Icons";
+import { useEntitySetContext } from "components/EntitySetProvider";
+import { useUIState } from "src/useUIState";
 
 export function DateTimeBlock(props: BlockProps) {
   let { rep } = useReplicache();
+  let { permissions } = useEntitySetContext();
   let date = useEntity(props.entityID, "block/date-time");
   let selectedDate = useMemo(() => {
     if (!date) return new Date();
     return new Date(date.data.value);
   }, [date]);
 
+  let isSelected = useUIState((s) =>
+    s.selectedBlocks.find((b) => b.value === props.entityID),
+  );
+
   return (
-    <Popover.Root>
+    <Popover.Root open={!permissions.write ? false : undefined}>
       <Popover.Trigger>
-        <button className="border">
-          {date ? selectedDate.toLocaleDateString() : "set date"}
+        <button
+          className={`flex flex-row gap-1 italic
+          ${isSelected ? "block-border" : "border border-transparent"}
+          `}
+        >
+          <BlockCalendarSmall />{" "}
+          {date ? (
+            selectedDate.toLocaleDateString(undefined, {
+              month: "short",
+              year: "numeric",
+              day: "numeric",
+            })
+          ) : (
+            <div className="italic rounded-md border border-border px-1 text-border w-48 text-left">
+              {permissions.write ? "add a date..." : "TBD..."}
+            </div>
+          )}
         </button>
       </Popover.Trigger>
 
       <Popover.Portal>
-        <Popover.Content>
+        <Popover.Content
+          className="w-48"
+          sideOffset={4}
+          align="start"
+          alignOffset={28}
+        >
           <DayPicker
+            classNames={{
+              selected: "bg-accent-1",
+              root: "bg-bg-page border p-2 rounded-md border-border",
+            }}
             mode="single"
             selected={selectedDate}
             onSelect={(date) => {
@@ -32,10 +64,6 @@ export function DateTimeBlock(props: BlockProps) {
                   data: { type: "string", value: date.toISOString() },
                   attribute: "block/date-time",
                 });
-            }}
-            classNames={{
-              selected: "bg-accent-1",
-              root: "bg-bg-page border",
             }}
           />
         </Popover.Content>
