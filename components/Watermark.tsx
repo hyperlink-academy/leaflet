ade5ac9b04ed0d57a83de045237ea0ed56d0b0a7
celine <celine@hyperlink.academy>
2024-11-11 17:33:02 -0500

    removed an underline in link on hover

--- components/Watermark.tsx
+++ components/Watermark.tsx
@@ -4,10 +4,10 @@ import { theme } from "tailwind.config";
 
 export const Watermark = (props: { mobile?: boolean }) => {
   return (
-    <Link href="/">
+    <Link href="/" className="hover:no-underline">
       <div className="sm:mb-2 sm:mr-4 group/watermark flex sm:flex-col gap-2 items-center justify-center ">
         <div
-          className="hidden group-hover/watermark:block sm:rotate-180 sm:py-1 sm:px-0 px-1 w-max rounded-md h-fit whitespace-nowrap text-sm  text-tertiary  hover:no-underline"
+          className="hidden group-hover/watermark:block sm:rotate-180 sm:py-1 sm:px-0 px-1 w-max rounded-md h-fit whitespace-nowrap text-sm  text-tertiary"
           style={{
             writingMode: !props.mobile ? "vertical-lr" : "horizontal-tb",
             backgroundColor: "rgba(var(--bg-page), 0.6)",
