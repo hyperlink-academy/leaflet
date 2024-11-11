9e81754a88708161e6e053b43e60bbb33f0b5af9
celine <celine@hyperlink.academy>
2024-11-11 17:02:09 -0500

    icon based watermark

--- components/Watermark.tsx
+++ components/Watermark.tsx
@@ -1,14 +1,21 @@
 import Link from "next/link";
+import { LogoSmall, LogoTiny } from "./Icons";
+import { theme } from "tailwind.config";
 
 export const Watermark = (props: { mobile?: boolean }) => {
   return (
-    <div
-      className="whitespace-nowrap text-xs sm:rotate-180 text-tertiary sm:pt-2 mr-0 "
-      style={!props.mobile ? { writingMode: "vertical-lr" } : {}}
-    >
-      made using{" "}
-      <Link className="font-bold" href="/">
-        Leaflet
+    <div className="pb-2 pr-2 group/watermark">
+      <div
+        className="hidden  group-hover/watermark:block whitespace-nowrap text-sm sm:rotate-180 text-tertiary sm:pt-2 mr-0"
+        style={!props.mobile ? { writingMode: "vertical-lr" } : {}}
+      >
+        made using <span className=" font-bold text-accent-1">Leaflet</span>
+      </div>
+      <Link
+        className="font-bold text-border group-hover/watermark:text-accent-1"
+        href="/"
+      >
+        <LogoSmall strokeColor={theme.colors["bg-page"]} />
       </Link>
     </div>
   );
