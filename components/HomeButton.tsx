e96b3c1e38fc648b8b66cc524c07b05152c61183
celine <celine@hyperlink.academy>
2025-01-17 16:56:09 -0500

    rm see full leaflet button on subpages to reduce complexity and mobile vs desktop consistancy issues

--- components/HomeButton.tsx
+++ components/HomeButton.tsx
@@ -1,41 +1,24 @@
 import Link from "next/link";
 import { useEntitySetContext } from "./EntitySetProvider";
-import { BackToLeafletSmall, HomeSmall } from "./Icons";
+import { HomeSmall } from "./Icons";
 import { HoverButton } from "./Buttons";
-import { useState } from "react";
 import { useParams, useSearchParams } from "next/navigation";
 
 export function HomeButton() {
   let { permissions } = useEntitySetContext();
   let searchParams = useSearchParams();
-  let params = useParams();
-  let isSubpage = !!searchParams.get("page");
 
   if (permissions.write)
     return (
-      <>
-        {isSubpage && (
-          <Link href={`/${params.leaflet_id}`} prefetch>
-            <HoverButton
-              noLabelOnMobile
-              icon={<BackToLeafletSmall />}
-              label="See Full Leaflet"
-              background="bg-accent-1"
-              text="text-accent-2"
-            />
-          </Link>
-        )}
-
-        <Link href="/home" prefetch>
-          <HoverButton
-            noLabelOnMobile
-            icon={<HomeSmall />}
-            label="Go Home"
-            background="bg-accent-1"
-            text="text-accent-2"
-          />
-        </Link>
-      </>
+      <Link href="/home" prefetch>
+        <HoverButton
+          noLabelOnMobile
+          icon={<HomeSmall />}
+          label="Go Home"
+          background="bg-accent-1"
+          text="text-accent-2"
+        />
+      </Link>
     );
   return null;
 }
