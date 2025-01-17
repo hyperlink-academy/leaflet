0bc3d445f5bc680075e2459f0b281f8a884fde45
Jared Pereira <jared@awarm.space>
2025-01-17 12:37:06 -0500

    add custom_domain tables

---
+++ supabase/migrations/20250117172835_add_custom_domain_tables.sql
@@ -0,0 +1,125 @@
+create table "public"."custom_domain_routes" (
+    "id" uuid not null default gen_random_uuid(),
+    "domain" text not null,
+    "route" text not null
+);
+
+
+create table "public"."custom_domains" (
+    "domain" text not null,
+    "identity" text not null default ''::text,
+    "confirmed" boolean not null
+);
+
+
+alter table "public"."custom_domains" enable row level security;
+
+alter table "public"."phone_rsvps_to_entity" add column "plus_ones" smallint not null default '0'::smallint;
+
+CREATE UNIQUE INDEX custom_domain_routes_domain_route_key ON public.custom_domain_routes USING btree (domain, route);
+
+CREATE UNIQUE INDEX custom_domain_routes_pkey ON public.custom_domain_routes USING btree (id);
+
+CREATE UNIQUE INDEX custom_domains_pkey ON public.custom_domains USING btree (domain);
+
+CREATE UNIQUE INDEX identities_email_key ON public.identities USING btree (email);
+
+alter table "public"."custom_domain_routes" add constraint "custom_domain_routes_pkey" PRIMARY KEY using index "custom_domain_routes_pkey";
+
+alter table "public"."custom_domains" add constraint "custom_domains_pkey" PRIMARY KEY using index "custom_domains_pkey";
+
+alter table "public"."custom_domain_routes" add constraint "custom_domain_routes_domain_fkey" FOREIGN KEY (domain) REFERENCES custom_domains(domain) not valid;
+
+alter table "public"."custom_domain_routes" validate constraint "custom_domain_routes_domain_fkey";
+
+alter table "public"."custom_domain_routes" add constraint "custom_domain_routes_domain_route_key" UNIQUE using index "custom_domain_routes_domain_route_key";
+
+alter table "public"."custom_domains" add constraint "custom_domains_identity_fkey" FOREIGN KEY (identity) REFERENCES identities(email) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+
+alter table "public"."custom_domains" validate constraint "custom_domains_identity_fkey";
+
+alter table "public"."identities" add constraint "identities_email_key" UNIQUE using index "identities_email_key";
+
+grant delete on table "public"."custom_domain_routes" to "anon";
+
+grant insert on table "public"."custom_domain_routes" to "anon";
+
+grant references on table "public"."custom_domain_routes" to "anon";
+
+grant select on table "public"."custom_domain_routes" to "anon";
+
+grant trigger on table "public"."custom_domain_routes" to "anon";
+
+grant truncate on table "public"."custom_domain_routes" to "anon";
+
+grant update on table "public"."custom_domain_routes" to "anon";
+
+grant delete on table "public"."custom_domain_routes" to "authenticated";
+
+grant insert on table "public"."custom_domain_routes" to "authenticated";
+
+grant references on table "public"."custom_domain_routes" to "authenticated";
+
+grant select on table "public"."custom_domain_routes" to "authenticated";
+
+grant trigger on table "public"."custom_domain_routes" to "authenticated";
+
+grant truncate on table "public"."custom_domain_routes" to "authenticated";
+
+grant update on table "public"."custom_domain_routes" to "authenticated";
+
+grant delete on table "public"."custom_domain_routes" to "service_role";
+
+grant insert on table "public"."custom_domain_routes" to "service_role";
+
+grant references on table "public"."custom_domain_routes" to "service_role";
+
+grant select on table "public"."custom_domain_routes" to "service_role";
+
+grant trigger on table "public"."custom_domain_routes" to "service_role";
+
+grant truncate on table "public"."custom_domain_routes" to "service_role";
+
+grant update on table "public"."custom_domain_routes" to "service_role";
+
+grant delete on table "public"."custom_domains" to "anon";
+
+grant insert on table "public"."custom_domains" to "anon";
+
+grant references on table "public"."custom_domains" to "anon";
+
+grant select on table "public"."custom_domains" to "anon";
+
+grant trigger on table "public"."custom_domains" to "anon";
+
+grant truncate on table "public"."custom_domains" to "anon";
+
+grant update on table "public"."custom_domains" to "anon";
+
+grant delete on table "public"."custom_domains" to "authenticated";
+
+grant insert on table "public"."custom_domains" to "authenticated";
+
+grant references on table "public"."custom_domains" to "authenticated";
+
+grant select on table "public"."custom_domains" to "authenticated";
+
+grant trigger on table "public"."custom_domains" to "authenticated";
+
+grant truncate on table "public"."custom_domains" to "authenticated";
+
+grant update on table "public"."custom_domains" to "authenticated";
+
+grant delete on table "public"."custom_domains" to "service_role";
+
+grant insert on table "public"."custom_domains" to "service_role";
+
+grant references on table "public"."custom_domains" to "service_role";
+
+grant select on table "public"."custom_domains" to "service_role";
+
+grant trigger on table "public"."custom_domains" to "service_role";
+
+grant truncate on table "public"."custom_domains" to "service_role";
+
+grant update on table "public"."custom_domains" to "service_role";
