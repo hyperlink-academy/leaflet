0cd23531decfcb4739ef802859af8666b4a44ae8
Brendan Schlagel <brendan.schlagel@gmail.com>
2025-01-23 21:22:21 -0500

    tiny copy tweaks to not-fix

--- app/not-found/page.tsx
+++ app/not-found/page.tsx
@@ -3,11 +3,11 @@ export default function NotFound() {
     <div className="w-screen h-screen flex place-items-center bg-bg-leaflet">
       <div className="bg-bg-page mx-auto p-4 border border-border rounded-md flex flex-col text-center justify-centergap-1 w-fit">
         <div className="font-bold">
-          Hmmm... Couldn&apos;t find that leaflet.
+          Hmmm... Couldn&apos;t find that Leaflet.
         </div>
         <div>
           You can{" "}
-          <a href="mailto:contact@hyperlink.academy" target="blank">
+          <a href="mailto:contact@leaflet.pub" target="blank">
             email us
           </a>{" "}
           for help!
