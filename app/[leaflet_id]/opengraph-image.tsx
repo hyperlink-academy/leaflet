c86538d6f3d0d831af9b311d22854f93e4b58d21
Jared Pereira <jared@awarm.space>
2025-01-24 23:02:54 -0500

    revalidate og-images every 60s

--- app/[leaflet_id]/opengraph-image.tsx
+++ app/[leaflet_id]/opengraph-image.tsx
@@ -1,6 +1,9 @@
 import { headers } from "next/headers";
 import { ImageResponse } from "next/og";
+
 export const runtime = "edge";
+export const revalidate = 60;
+
 export default async function OpenGraphImage(props: {
   params: { leaflet_id: string };
 }) {
@@ -10,7 +13,7 @@ export default async function OpenGraphImage(props: {
   let protocol = headersList.get("x-forwarded-proto");
   let path = `${protocol}://${hostname}/${props.params.leaflet_id}`;
   let response = await fetch(
-    `https://pro.microlink.io/?url=${path}&screenshot=true&viewport.width=1400&viewport.height=733&meta=false&embed=screenshot.url`,
+    `https://pro.microlink.io/?url=${path}&screenshot=true&viewport.width=1400&viewport.height=733&meta=false&embed=screenshot.url&force=true`,
     {
       headers: {
         "x-api-key": process.env.MICROLINK_API_KEY!,
