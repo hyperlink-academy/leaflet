f3beb9babecf0e7f8b6fb6ccbca089dccbe3c3f4
Jared Pereira <jared@awarm.space>
2025-02-07 17:31:01 -0500

    redirect / => home if auth'd and add /new

--- app/new/route.ts
+++ app/new/route.ts
@@ -1,13 +1,9 @@
 import { createNewLeaflet } from "actions/createNewLeaflet";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 export const preferredRegion = ["sfo1"];
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
 export async function GET() {
-  let auth_token = cookies().get("auth_token")?.value;
-  if (auth_token) redirect("/home");
-  else await createNewLeaflet("doc", true);
+  await createNewLeaflet("doc", true);
 }
