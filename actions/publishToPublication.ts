4e5f6232d3557c1cc02695a35e6205c8ddf8c3f9
Jared Pereira <jared@awarm.space>
2025-03-22 09:25:17 -0700

    add email to subscribers!

--- actions/publishToPublication.ts
+++ actions/publishToPublication.ts
@@ -22,6 +22,7 @@ import { ids } from "lexicons/src/lexicons";
 import { OmitKey } from "lexicons/src/util";
 import { BlobRef } from "@atproto/lexicon";
 import { IdResolver } from "@atproto/identity";
+import { AtUri } from "@atproto/syntax";
 
 const idResolver = new IdResolver();
 export async function publishToPublication(
@@ -74,7 +75,50 @@ export async function publishToPublication(
   let title = "Untitled";
   let titleBlock = blocks.find((f) => f.type === "heading");
   if (titleBlock) title = getBlockContent(titleBlock.value);
-  let b: PubLeafletPagesLinearDocument.Block[] = blocks.flatMap((b) => {
+  let b: PubLeafletPagesLinearDocument.Block[] = blocksToRecord(
+    blocks,
+    imageMap,
+    scan,
+  );
+
+  let record: OmitKey<PubLeafletDocument.Record, "$type"> = {
+    author: credentialSession.did!,
+    title,
+    publication: publication_uri,
+    pages: [
+      {
+        $type: "pub.leaflet.pages.linearDocument",
+        blocks: b,
+      },
+    ],
+  };
+  let rkey = TID.nextStr();
+  let result = await agent.pub.leaflet.document.create(
+    { repo: credentialSession.did!, rkey, validate: false },
+    record,
+  );
+  let html = blocksToHtml(blocks, imageMap, scan, publication_uri);
+  await sendPostToEmailSubscribers(publication_uri, { title, content: html });
+  let handle = await idResolver.did.resolve(credentialSession.did!);
+  return { handle, rkey };
+}
+
+function blocksToRecord(
+  blocks: Block[],
+  imageMap: Map<string, BlobRef>,
+  scan: ReturnType<typeof scanIndexLocal>,
+) {
+  const getBlockContent = (b: string) => {
+    let [content] = scan.eav(b, "block/text");
+    if (!content) return "";
+    let doc = new Y.Doc();
+    const update = base64.toByteArray(content.data.value);
+    Y.applyUpdate(doc, update);
+    let nodes = doc.getXmlElement("prosemirror").toArray();
+    let stringValue = YJSFragmentToString(nodes[0]);
+    return stringValue;
+  };
+  return blocks.flatMap((b) => {
     if (b.type !== "text" && b.type !== "heading" && b.type !== "image")
       return [];
     if (b.type === "heading") {
@@ -102,7 +146,7 @@ export async function publishToPublication(
             $type: ids.PubLeafletBlocksText,
             plaintext: stringValue,
           },
-        },
+        } as PubLeafletPagesLinearDocument.Block,
       ];
     }
     if (b.type == "image") {
@@ -121,28 +165,103 @@ export async function publishToPublication(
               width: image.data.width,
             },
           },
-        },
+        } as PubLeafletPagesLinearDocument.Block,
       ];
     }
     return [];
   });
+}
 
-  let record: OmitKey<PubLeafletDocument.Record, "$type"> = {
-    author: credentialSession.did!,
-    title,
-    publication: publication_uri,
-    pages: [
-      {
-        $type: "pub.leaflet.pages.linearDocument",
-        blocks: b,
-      },
-    ],
+function blocksToHtml(
+  blocks: Block[],
+  imageMap: Map<string, BlobRef>,
+  scan: ReturnType<typeof scanIndexLocal>,
+  publication_uri: string,
+) {
+  const getBlockContent = (b: string) => {
+    let [content] = scan.eav(b, "block/text");
+    if (!content) return "";
+    let doc = new Y.Doc();
+    const update = base64.toByteArray(content.data.value);
+    Y.applyUpdate(doc, update);
+    let nodes = doc.getXmlElement("prosemirror").toArray();
+    let stringValue = YJSFragmentToString(nodes[0]);
+    return stringValue;
   };
-  let rkey = TID.nextStr();
-  let result = await agent.pub.leaflet.document.create(
-    { repo: credentialSession.did!, rkey, validate: false },
-    record,
-  );
-  let handle = await idResolver.did.resolve(credentialSession.did!);
-  return { handle, rkey };
+  return blocks
+    .flatMap((b) => {
+      if (b.type !== "text" && b.type !== "heading" && b.type !== "image")
+        return [];
+      if (b.type === "heading") {
+        let [headingLevel] = scan.eav(b.value, "block/heading-level");
+
+        let stringValue = getBlockContent(b.value);
+        let l = headingLevel?.data.value || 1;
+        return [`<h${l}>${stringValue}</h${l}>`];
+      }
+
+      if (b.type == "text") {
+        let stringValue = getBlockContent(b.value);
+        return `<p>${stringValue}</p>`;
+      }
+      if (b.type == "image") {
+        let [image] = scan.eav(b.value, "block/image");
+        if (!image) return [];
+        let blobref = imageMap.get(image.data.src);
+        if (!blobref) return [];
+        let uri = new AtUri(publication_uri);
+        return `<img
+        height=${image.data.height}
+        width=${image.data.width}>
+        src="https://bsky.social/xrpc/com.atproto.sync.getBlob?did=${uri.hostname}&cid=${(blobref as unknown as { $link: string })["$link"]}"
+        </img>`;
+      }
+      return [""];
+    })
+    .join("\n");
+}
+
+async function sendPostToEmailSubscribers(
+  publication_uri: string,
+  post: { content: string; title: string },
+) {
+  let { data: publication } = await supabaseServerClient
+    .from("publications")
+    .select("*, subscribers_to_publications(*)")
+    .eq("uri", publication_uri)
+    .single();
+
+  let res = await fetch("https://api.postmarkapp.com/email/batch", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Postmark-Server-Token": process.env.POSTMARK_API_KEY!,
+    },
+    body: JSON.stringify(
+      publication?.subscribers_to_publications.map((sub) => ({
+        Headers: [
+          {
+            Name: "List-Unsubscribe-Post",
+            Value: "List-Unsubscribe=One-Click",
+          },
+          {
+            Name: "List-Unsubscribe",
+            Value: `<${"TODO"}/mail/unsubscribe?sub_id=${sub.identity}>`,
+          },
+        ],
+        MessageStream: "broadcast",
+        From: `${publication.name} <mailbox@leaflet.pub>`,
+        Subject: post.title,
+        To: sub.identity,
+        HtmlBody: `
+        <h1>${publication.name}</h1>
+        <hr style="margin-top: 1em; margin-bottom: 1em;">
+        ${post.content}
+        <hr style="margin-top: 1em; margin-bottom: 1em;">
+        This is a super alpha release! Ask Jared if you want to unsubscribe (sorry)
+        `,
+        TextBody: post.content,
+      })),
+    ),
+  });
 }
