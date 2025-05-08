953802870d21425a59fede466e36c9d08bc80c9c
Jared Pereira <jared@awarm.space>
2025-05-08 15:50:38 -0400

    WIP action bar

--- app/[leaflet_id]/Sidebar.tsx
+++ app/[leaflet_id]/Sidebar.tsx
@@ -21,29 +21,31 @@ export function LeafletSidebar(props: { leaflet_id: string }) {
         e.currentTarget === e.target && blurPage();
       }}
     >
-      <Sidebar>
-        {entity_set.permissions.write ? (
-          <div className=" flex flex-col justify-center gap-2 mr-4">
-            {publication.publication && (
-              <div className="relative w-[30px] h-[76px]">
-                <div className="origin-top-left -rotate-90 absolute translate-y-[76px]">
-                  <PublishToPublication />
+      <div className="flex flex-col justify-between h-full pr-1">
+        <Sidebar>
+          {entity_set.permissions.write ? (
+            <div className=" flex flex-col justify-center gap-2 ">
+              {publication.publication && (
+                <div className="relative w-[30px] h-[76px]">
+                  <div className="origin-top-left -rotate-90 absolute translate-y-[76px]">
+                    <PublishToPublication />
+                  </div>
                 </div>
-              </div>
-            )}
-            <ShareOptions />
-            <ThemePopover entityID={props.leaflet_id} />
-            <HelpPopover />
-            <hr className="text-border my-3" />
-            <HomeButton />
-          </div>
-        ) : (
-          <div>
-            <HomeButton />
-          </div>
-        )}
+              )}
+              <ShareOptions />
+              <ThemePopover entityID={props.leaflet_id} />
+              <HelpPopover />
+              <hr className="text-border my-3" />
+              <HomeButton />
+            </div>
+          ) : (
+            <div>
+              <HomeButton />
+            </div>
+          )}
+        </Sidebar>
         <Watermark />
-      </Sidebar>
+      </div>
     </div>
   );
 }
--- components/ActionBar/Sidebar.tsx
+++ components/ActionBar/Sidebar.tsx
@@ -1,11 +1,20 @@
+import { ButtonPrimary } from "components/Buttons";
 import { Media } from "components/Media";
 
 export function Sidebar(props: { children?: React.ReactNode }) {
   return (
-    <Media mobile={false} className="h-full">
-      <div className="flex flex-col h-full justify-between  mt-1">
+    <Media mobile={false}>
+      <div className="flex flex-col justify-between  mt-1 border rounded-md border-bg-page bg-bg-page bg-opacity-50 h-fit items-center py-1 px-0.5 ">
         {props.children}
       </div>
     </Media>
   );
+}
+
+export function SidebarButton(props: {
+  icon: React.ReactNode;
+  content: React.ReactNode;
+  onClick: (e: React.MouseEvent) => void;
+}) {
+  return <button>{props.icon}</button>;
 }
