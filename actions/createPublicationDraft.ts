d20d76a42db84ef2570fac239a5524545945500b
Jared Pereira <jared@awarm.space>
2025-05-14 19:54:18 -0400

    make first block text in pub drafts

--- actions/createPublicationDraft.ts
+++ actions/createPublicationDraft.ts
@@ -6,7 +6,11 @@ import { supabaseServerClient } from "supabase/serverClient";
 export async function createPublicationDraft(publication_uri: string) {
   let identity = await getIdentityData();
   if (!identity || !identity.atp_did) return null;
-  let newLeaflet = await createNewLeaflet("doc", false);
+  let newLeaflet = await createNewLeaflet({
+    pageType: "doc",
+    redirectUser: false,
+    firstBlockType: "text",
+  });
   console.log(
     await supabaseServerClient
       .from("leaflets_in_publications")
