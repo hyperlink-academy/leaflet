ebc78156140e63c4635437473c9928a0e90413b2
Jared Pereira <jared@awarm.space>
2025-05-21 12:35:28 -0400

    use vercel_env for isProduction logic

--- src/utils/isProductionDeployment.ts
+++ src/utils/isProductionDeployment.ts
@@ -1,7 +1,4 @@
 export function isProductionDomain() {
-  let url =
-    process.env.NEXT_PUBLIC_VERCEL_URL ||
-    process.env.VERCEL_URL ||
-    "http://localhost:3000";
-  return process.env.NODE_ENV === "production" && url.includes("leaflet.pub");
+  let vercel_env = process.env.NEXT_PUBLIC_VERCEL_ENV || process.env.VERCEL_ENV;
+  return process.env.NODE_ENV === "production" && vercel_env === "production";
 }
