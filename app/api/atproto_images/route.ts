674fad811158d78abafb5d9bab5061ccbb9ad115
Jared Pereira <jared@awarm.space>
2025-05-22 10:57:12 -0400

    fix image route

--- app/api/atproto_images/route.ts
+++ app/api/atproto_images/route.ts
@@ -2,13 +2,19 @@ import { IdResolver } from "@atproto/identity";
 import { NextRequest, NextResponse } from "next/server";
 let idResolver = new IdResolver();
 
-export async function GET(
-  req: NextRequest,
-  props: { params: Promise<{ did: string; cid?: string }> },
-) {
-  let params = await props.params;
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const params = {
+    did: url.searchParams.get("did") ?? "",
+    cid: url.searchParams.get("cid") ?? "",
+  };
+  console.log(params);
+  if (!params.did || !params.cid)
+    return new NextResponse(null, { status: 404 });
+
   let identity = await idResolver.did.resolve(params.did);
-  let service = identity?.service?.find((f) => f.type === "atproto_pds");
+  let service = identity?.service?.find((f) => f.id === "#atproto_pds");
+  console.log(identity);
   if (!service) return new NextResponse(null, { status: 404 });
   return fetch(
     `${service.serviceEndpoint}/xrpc/com.atproto.sync.getBlob?did=${params.did}&cid=${params.cid}`,
