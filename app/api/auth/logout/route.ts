15b4b60c9c61d90488e00ad521c2cf6aa8654563
Jared Pereira <jared@awarm.space>
2025-05-29 16:19:41 -0400

    add functions to middleware for cross-sit authentication (#138)
    * add functions to middleware for cross-sit authentication
    
    * use constants for cross site auth route names

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest } from "next/server";
+import { supabaseServerClient } from "supabase/serverClient";
 export const runtime = "edge";
 export const preferredRegion = [];
+export const dynamic = "force-dynamic";
 
 export async function GET(req: NextRequest) {
   const host = req.headers.get("host");
@@ -13,6 +15,9 @@ export async function GET(req: NextRequest) {
 
   // Get the base domain from the host
   const domain = host?.includes(":") ? host.split(":")[0] : host;
+  let token = req.cookies.get("auth_token");
+  if (token)
+    supabaseServerClient.from("email_auth_tokens").delete().eq("id", token);
 
   // Clear the auth_token cookie on both the base domain and the domain with a leading dot
   response.headers.append(
