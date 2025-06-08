9fdc8ac084545f270e3b19e665fdfa56d40e695d
Jared Pereira <jared@awarm.space>
2025-06-08 15:06:54 -0400

    decode uri component for pub pages

--- app/lish/[did]/[publication]/[rkey]/opengraph-image.ts
+++ app/lish/[did]/[publication]/[rkey]/opengraph-image.ts
@@ -7,6 +7,6 @@ export default async function OpenGraphImage(props: {
   params: { publication: string; did: string; rkey: string };
 }) {
   return getMicroLinkOgImage(
-    `/lish/${props.params.did}/${props.params.publication}/${props.params.rkey}`,
+    `/lish/${decodeURIComponent(props.params.did)}/${decodeURIComponent(props.params.publication)}/${props.params.rkey}`,
   );
 }
--- app/lish/[did]/[publication]/opengraph-image.ts
+++ app/lish/[did]/[publication]/opengraph-image.ts
@@ -7,6 +7,6 @@ export default async function OpenGraphImage(props: {
   params: { publication: string; did: string };
 }) {
   return getMicroLinkOgImage(
-    `/lish/${props.params.did}/${props.params.publication}`,
+    `/lish/${decodeURIComponent(props.params.did)}/${decodeURIComponent(props.params.publication)}`,
   );
 }
