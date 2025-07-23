deeb02b3dd174a367990c576fd5da2fc52b43445
celine <celine@hyperlink.academy>
2025-07-23 19:32:02 -0400

    reworked the subscriber list tab

--- app/lish/[did]/[publication]/dashboard/PublicationSubscribers.tsx
+++ app/lish/[did]/[publication]/dashboard/PublicationSubscribers.tsx
@@ -6,11 +6,40 @@ import { ButtonPrimary } from "components/Buttons";
 import { getPublicationURL } from "app/lish/createPub/getPublicationURL";
 import { useSmoker } from "components/Toast";
 import { BlueskyTiny } from "components/Icons/BlueskyTiny";
-import { Separator } from "components/Layout";
+import { Menu, MenuItem, Separator } from "components/Layout";
+import { MoreOptionsTiny } from "components/Icons/MoreOptionsTiny";
+import { MoreOptionsVerticalTiny } from "components/Icons/MoreOptionsVerticalTiny";
+import { Checkbox } from "components/Checkbox";
+import { useEffect, useState } from "react";
+import { Popover } from "components/Popover";
+
+type subscriber = { email: string | undefined; handle: string | undefined };
+let subscribers: subscriber[] = [
+  { email: "thisiscelinepark@gmail.com", handle: "cozylittle.house" },
+  { email: "jared@awarm.space", handle: "awarm.space" },
+  { email: "brendan.schlagel@gmail.com", handle: "schlage.town" },
+];
 
 export function PublicationSubscribers() {
   let { data: publication } = usePublicationData();
-  let smoker = useSmoker();
+  let [checkedSubscribers, setCheckedSubscribers] = useState<subscriber[]>([]);
+  let [checkAll, setCheckAll] = useState(false);
+
+  useEffect(() => {
+    const allSubscribersSelected = subscribers.every((subscriber) =>
+      checkedSubscribers.some(
+        (checked) =>
+          checked.email === subscriber.email &&
+          checked.handle === subscriber.handle,
+      ),
+    );
+
+    if (allSubscribersSelected && subscribers.length > 0) {
+      setCheckAll(true);
+    } else {
+      setCheckAll(false);
+    }
+  }, [checkedSubscribers]);
 
   if (!publication) return <div>null</div>;
   // if (publication.publication_subscriptions.length === 0)
@@ -39,23 +68,52 @@ export function PublicationSubscribers() {
   //   );
 
   return (
-    <div className="flex gap-2 flex-col">
-      <DummySubscriber
-        handle="@cozylittle.house"
-        displayName="celine"
-        email="thisiscelinepark@gmail.com"
-      />
-      <DummySubscriber
-        handle="@awarm.space"
-        displayName="jared"
-        email="jared@awarm.space"
-      />
-      <DummySubscriber
-        handle="@schlage.town"
-        displayName="Brendan"
-        email="brendan.schlagel@gmail.com"
-      />
-      {/* {publication.publication_subscriptions
+    <div>
+      <div className="subscriberListHeader flex gap-2 ">
+        <Checkbox
+          checked={checkAll}
+          onChange={() => {
+            if (checkAll === false) {
+              const allSubscribers = subscribers.map((subscriber) => ({
+                email: subscriber.email,
+                handle: subscriber.handle,
+              }));
+              setCheckedSubscribers(allSubscribers);
+            } else {
+              setCheckedSubscribers([]);
+            }
+          }}
+          className="!font-bold text-secondary mb-1"
+        >
+          {subscribers.length} Subscriber{subscribers.length !== 1 && "s"}
+        </Checkbox>
+        {checkedSubscribers.length !== 0 && (
+          <SubscriberOptions
+            checkedSubscribers={checkedSubscribers}
+            allSelected={checkedSubscribers.length === subscribers.length}
+          />
+        )}
+      </div>
+      <hr className="mb-2 border-border" />
+      <div className="subscriberListContent flex gap-3 flex-col">
+        {subscribers.map((subscriber) => {
+          return (
+            <DummySubscriber
+              key={`${subscriber.email}-${subscriber.handle}`}
+              handle={subscriber.handle}
+              email={subscriber.email}
+              checkedSubscribers={checkedSubscribers}
+              setCheckedSubscribers={setCheckedSubscribers}
+              checked={checkedSubscribers.some(
+                (s) =>
+                  s.email === subscriber.email &&
+                  s.handle === subscriber.handle,
+              )}
+            />
+          );
+        })}
+
+        {/* {publication.publication_subscriptions
           .sort((a, b) => {
             return b.created_at.localeCompare(a.created_at);
           })
@@ -74,42 +132,73 @@ export function PublicationSubscribers() {
               />
             );
           })} */}
+      </div>
     </div>
   );
 }
 
 const DummySubscriber = (props: {
-  displayName: string;
-  handle: string;
-  email: string;
+  handle: string | undefined;
+  email: string | undefined;
+  checkedSubscribers: subscriber[];
+  setCheckedSubscribers: (subscribers: subscriber[]) => void;
+  checked: boolean;
 }) => {
   return (
-    <div>
-      <a
-        target="_blank"
-        href={"/"}
-        className="flex text-primary p-2 gap-1 hover:!no-underline"
-      >
-        <div className="flex flex-row gap-2">
-          <div className="rounded-full w-5 h-5 bg-test mt-[2px]" />
-          <div className="flex flex-col">
-            <p className="font-bold">{props.displayName}</p>
-            <div className="flex gap-3">
-              <p className="text-secondary text-sm flex gap-2 items-center">
-                <BlueskyTiny />
-                {props.email}
-              </p>
-              <Separator />
-              <p className="text-secondary text-sm flex gap-2 items-center">
-                <BlueskyTiny />
-                {props.handle}
-              </p>
-            </div>
-          </div>
+    <Checkbox
+      className="!font-normal"
+      checked={props.checked}
+      onChange={() => {
+        if (props.checked === false) {
+          const newCheckedSubscribers = [
+            ...props.checkedSubscribers,
+            {
+              email: props.email,
+              handle: props.handle,
+            },
+          ];
+          props.setCheckedSubscribers(newCheckedSubscribers);
+        } else {
+          const newCheckedSubscribers = props.checkedSubscribers.filter(
+            (subscriber) =>
+              !(
+                subscriber.email === props.email &&
+                subscriber.handle === props.handle
+              ),
+          );
+          props.setCheckedSubscribers(newCheckedSubscribers);
+        }
+        console.log(props.checkedSubscribers);
+      }}
+    >
+      <div className=" flex justify-between w-full">
+        <div className="flex flex-col gap-0 sm:flex-row sm:gap-2">
+          <a
+            target="_blank"
+            href={"mailto:/"}
+            className="font-bold  text-primary"
+          >
+            {props.email}
+          </a>
+
+          {props.handle && props.email && (
+            <Separator classname="sm:block hidden" />
+          )}
+          {props.handle && (
+            <a
+              target="_blank"
+              href={"mailto:/"}
+              className={` ${!props.email ? "font-bold text-primary" : "text-tertiary"}`}
+            >
+              @{props.handle}
+            </a>
+          )}
         </div>
-      </a>
-      <hr className="border-border last:hidden" />
-    </div>
+      </div>
+      <div className="px-1 py-0 h-max rounded-md accent-container border border-border text-tertiary">
+        Free
+      </div>
+    </Checkbox>
   );
 };
 
@@ -144,5 +233,30 @@ const Subscriber = (props: {
       </a>
       <hr className="border-border last:hidden" />
     </div>
+  );
+};
+
+const SubscriberOptions = (props: {
+  checkedSubscribers: subscriber[];
+  allSelected: boolean;
+}) => {
+  return (
+    <Menu
+      asChild
+      className=""
+      trigger={
+        <ButtonPrimary compact className="-mt-[1px]">
+          {props.allSelected ? "All" : props.checkedSubscribers.length} Selected{" "}
+          <MoreOptionsVerticalTiny />
+        </ButtonPrimary>
+      }
+    >
+      <MenuItem className="justify-center" onSelect={() => {}}>
+        Export {props.allSelected ? "All" : "Selected"}
+      </MenuItem>
+      <MenuItem className="justify-center" onSelect={() => {}}>
+        Remove {props.allSelected ? "All" : "Selected"}
+      </MenuItem>
+    </Menu>
   );
 };
