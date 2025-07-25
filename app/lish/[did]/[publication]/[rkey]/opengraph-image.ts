6ab0900689595a232a143afdb7cae9d81eb1b8d3
Jared Pereira <jared@awarm.space>
2025-07-24 23:44:18 -0400

    use old og-image as we don't have searchParams

--- app/lish/[did]/[publication]/[rkey]/opengraph-image.ts
+++ app/lish/[did]/[publication]/[rkey]/opengraph-image.ts
@@ -5,22 +5,8 @@ export const revalidate = 60;
 
 export default async function OpenGraphImage(props: {
   params: { publication: string; did: string; rkey: string };
-  searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const searchParams = new URLSearchParams();
-  Object.entries(props.searchParams).forEach(([key, value]) => {
-    if (value !== undefined) {
-      if (Array.isArray(value)) {
-        value.forEach((v) => searchParams.append(key, v));
-      } else {
-        searchParams.append(key, value);
-      }
-    }
-  });
-  const queryString = searchParams.toString();
-  const url =
-    `/lish/${decodeURIComponent(props.params.did)}/${decodeURIComponent(props.params.publication)}/${props.params.rkey}/` +
-    (queryString ? `?${queryString}` : "");
-
-  return getMicroLinkOgImage(url);
+  return getMicroLinkOgImage(
+    `/lish/${decodeURIComponent(props.params.did)}/${decodeURIComponent(props.params.publication)}/${props.params.rkey}/`,
+  );
 }
