295c8282acc7d7bb8309825ecdad0cc76b9f0438
Jared Pereira <jared@awarm.space>
2025-07-24 23:22:19 -0400

    pass searchparams to microlink for og images

--- app/lish/[did]/[publication]/[rkey]/opengraph-image.ts
+++ app/lish/[did]/[publication]/[rkey]/opengraph-image.ts
@@ -5,8 +5,22 @@ export const revalidate = 60;
 
 export default async function OpenGraphImage(props: {
   params: { publication: string; did: string; rkey: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  return getMicroLinkOgImage(
-    `/lish/${decodeURIComponent(props.params.did)}/${decodeURIComponent(props.params.publication)}/${props.params.rkey}/`,
-  );
+  const searchParams = new URLSearchParams();
+  Object.entries(props.searchParams).forEach(([key, value]) => {
+    if (value !== undefined) {
+      if (Array.isArray(value)) {
+        value.forEach((v) => searchParams.append(key, v));
+      } else {
+        searchParams.append(key, value);
+      }
+    }
+  });
+  const queryString = searchParams.toString();
+  const url =
+    `/lish/${decodeURIComponent(props.params.did)}/${decodeURIComponent(props.params.publication)}/${props.params.rkey}/` +
+    (queryString ? `?${queryString}` : "");
+
+  return getMicroLinkOgImage(url);
 }
