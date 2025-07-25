69ed8e21dfce454dcef0745b2afbaadb86110d16
Jared Pereira <jared@awarm.space>
2025-07-25 16:22:36 -0400

    add metadata to quote pages

--- app/lish/[did]/[publication]/[rkey]/quote/[quote]/page.tsx
+++ app/lish/[did]/[publication]/[rkey]/quote/[quote]/page.tsx
@@ -1,4 +1,8 @@
-import PostPage from "app/lish/[did]/[publication]/[rkey]/page";
+import PostPage, {
+  generateMetadata,
+} from "app/lish/[did]/[publication]/[rkey]/page";
+
+export { generateMetadata } from "app/lish/[did]/[publication]/[rkey]/page";
 export default async function Post(props: {
   params: Promise<{ publication: string; did: string; rkey: string }>;
 }) {
