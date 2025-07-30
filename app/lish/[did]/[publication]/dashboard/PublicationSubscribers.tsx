263b82a9912a1289a016fad2b981698746bbc2da
celine <celine@hyperlink.academy>
2025-07-30 00:13:50 -0400

    wired the subscribers! except for email stuff

--- app/lish/[did]/[publication]/dashboard/PublicationSubscribers.tsx
+++ app/lish/[did]/[publication]/dashboard/PublicationSubscribers.tsx
@@ -1,36 +1,31 @@
 "use client";
 import { AppBskyActorProfile } from "lexicons/api";
 import { usePublicationData } from "./PublicationSWRProvider";
-import { blobRefToSrc } from "src/utils/blobRefToSrc";
 import { ButtonPrimary } from "components/Buttons";
 import { getPublicationURL } from "app/lish/createPub/getPublicationURL";
 import { useSmoker } from "components/Toast";
-import { BlueskyTiny } from "components/Icons/BlueskyTiny";
 import { Menu, MenuItem, Separator } from "components/Layout";
-import { MoreOptionsTiny } from "components/Icons/MoreOptionsTiny";
 import { MoreOptionsVerticalTiny } from "components/Icons/MoreOptionsVerticalTiny";
 import { Checkbox } from "components/Checkbox";
 import { useEffect, useState } from "react";
-import { Popover } from "components/Popover";
 
-type subscriber = { email: string | undefined; handle: string | undefined };
-let subscribers: subscriber[] = [
-  { email: "thisiscelinepark@gmail.com", handle: "cozylittle.house" },
-  { email: "jared@awarm.space", handle: "awarm.space" },
-  { email: "brendan.schlagel@gmail.com", handle: "schlage.town" },
-];
+type subscriber = { email: string | undefined; did: string | undefined };
 
 export function PublicationSubscribers() {
+  let smoker = useSmoker();
   let { data: publication } = usePublicationData();
   let [checkedSubscribers, setCheckedSubscribers] = useState<subscriber[]>([]);
   let [checkAll, setCheckAll] = useState(false);
+
+  if (!publication) return <div>null</div>;
+  let subscribers = publication.publication_subscriptions;
 
   useEffect(() => {
     const allSubscribersSelected = subscribers.every((subscriber) =>
       checkedSubscribers.some(
         (checked) =>
-          checked.email === subscriber.email &&
-          checked.handle === subscriber.handle,
+          checked.email === "dummyemail@email.com" &&
+          checked.did === subscriber.identities?.bsky_profiles?.did,
       ),
     );
 
@@ -41,31 +36,30 @@ export function PublicationSubscribers() {
     }
   }, [checkedSubscribers]);
 
-  if (!publication) return <div>null</div>;
-  // if (publication.publication_subscriptions.length === 0)
-  //   return (
-  //     <div className="italic text-tertiary  flex flex-col gap-0 text-center justify-center pt-4">
-  //       <p className="font-bold"> No subscribers yet </p>
-  //       <p>Start sharing your publication!</p>
-  //       <ButtonPrimary
-  //         className="mx-auto mt-2"
-  //         onClick={(e) => {
-  //           e.preventDefault();
-  //           let rect = (e.currentTarget as Element)?.getBoundingClientRect();
-  //           navigator.clipboard.writeText(getPublicationURL(publication!));
-  //           smoker({
-  //             position: {
-  //               x: rect ? rect.left + (rect.right - rect.left) / 2 : 0,
-  //               y: rect ? rect.top + 26 : 0,
-  //             },
-  //             text: "Copied Publication URL!",
-  //           });
-  //         }}
-  //       >
-  //         Copy Share Link
-  //       </ButtonPrimary>
-  //     </div>
-  //   );
+  if (subscribers.length === 0)
+    return (
+      <div className="italic text-tertiary  flex flex-col gap-0 text-center justify-center pt-4">
+        <p className="font-bold"> No subscribers yet </p>
+        <p>Start sharing your publication!</p>
+        <ButtonPrimary
+          className="mx-auto mt-2"
+          onClick={(e) => {
+            e.preventDefault();
+            let rect = (e.currentTarget as Element)?.getBoundingClientRect();
+            navigator.clipboard.writeText(getPublicationURL(publication!));
+            smoker({
+              position: {
+                x: rect ? rect.left + (rect.right - rect.left) / 2 : 0,
+                y: rect ? rect.top + 26 : 0,
+              },
+              text: "Copied Publication URL!",
+            });
+          }}
+        >
+          Copy Share Link
+        </ButtonPrimary>
+      </div>
+    );
 
   return (
     <div>
@@ -75,8 +69,8 @@ export function PublicationSubscribers() {
           onChange={() => {
             if (checkAll === false) {
               const allSubscribers = subscribers.map((subscriber) => ({
-                email: subscriber.email,
-                handle: subscriber.handle,
+                email: "dummyemail@email.com",
+                did: subscriber.identities?.bsky_profiles?.did,
               }));
               setCheckedSubscribers(allSubscribers);
             } else {
@@ -96,50 +90,41 @@ export function PublicationSubscribers() {
       </div>
       <hr className="mb-2 border-border" />
       <div className="subscriberListContent flex gap-3 flex-col">
-        {subscribers.map((subscriber) => {
-          return (
-            <DummySubscriber
-              key={`${subscriber.email}-${subscriber.handle}`}
-              handle={subscriber.handle}
-              email={subscriber.email}
-              checkedSubscribers={checkedSubscribers}
-              setCheckedSubscribers={setCheckedSubscribers}
-              checked={checkedSubscribers.some(
-                (s) =>
-                  s.email === subscriber.email &&
-                  s.handle === subscriber.handle,
-              )}
-            />
-          );
-        })}
-
-        {/* {publication.publication_subscriptions
+        {subscribers
           .sort((a, b) => {
             return b.created_at.localeCompare(a.created_at);
           })
           .map((subscriber, index) => {
             if (!subscriber.identities?.bsky_profiles) return null;
             let handle = subscriber.identities?.bsky_profiles.handle;
+            let did = subscriber.identities?.bsky_profiles.did;
             let profile = subscriber.identities?.bsky_profiles
               ?.record as AppBskyActorProfile.Record | null;
             if (!profile) return null;
             return (
-              <Subscriber
-                key={subscriber.identities.bsky_profiles?.did}
-                profile={profile}
-                did={subscriber.identities.bsky_profiles?.did}
-                handle={handle}
+              // replace the dummy email with the subscriber email! i don't know how to get that data D:
+              <SubscriberListItem
+                key={`${subscriber.identities.bsky_profiles?.did}`}
+                handle={handle ? handle : undefined}
+                email={"dummyemail@email.com"}
+                did={`${subscriber.identities.bsky_profiles?.did}`}
+                checkedSubscribers={checkedSubscribers}
+                setCheckedSubscribers={setCheckedSubscribers}
+                checked={checkedSubscribers.some(
+                  (s) => s.email === "dummyemail@email.com" && s.did === did,
+                )}
               />
             );
-          })} */}
+          })}
       </div>
     </div>
   );
 }
 
-const DummySubscriber = (props: {
+const SubscriberListItem = (props: {
   handle: string | undefined;
   email: string | undefined;
+  did: string | undefined;
   checkedSubscribers: subscriber[];
   setCheckedSubscribers: (subscribers: subscriber[]) => void;
   checked: boolean;
@@ -154,7 +139,7 @@ const DummySubscriber = (props: {
             ...props.checkedSubscribers,
             {
               email: props.email,
-              handle: props.handle,
+              did: props.did,
             },
           ];
           props.setCheckedSubscribers(newCheckedSubscribers);
@@ -162,8 +147,7 @@ const DummySubscriber = (props: {
           const newCheckedSubscribers = props.checkedSubscribers.filter(
             (subscriber) =>
               !(
-                subscriber.email === props.email &&
-                subscriber.handle === props.handle
+                subscriber.email === props.email && subscriber.did === props.did
               ),
           );
           props.setCheckedSubscribers(newCheckedSubscribers);
@@ -175,7 +159,7 @@ const DummySubscriber = (props: {
         <div className="flex flex-col gap-0 sm:flex-row sm:gap-2">
           <a
             target="_blank"
-            href={"mailto:/"}
+            href={`mailto:${props.email}`}
             className="font-bold  text-primary"
           >
             {props.email}
@@ -187,7 +171,7 @@ const DummySubscriber = (props: {
           {props.handle && (
             <a
               target="_blank"
-              href={"mailto:/"}
+              href={`https://bsky.app/profile/${props.did}`}
               className={` ${!props.email ? "font-bold text-primary" : "text-tertiary"}`}
             >
               @{props.handle}
@@ -196,43 +180,9 @@ const DummySubscriber = (props: {
         </div>
       </div>
       <div className="px-1 py-0 h-max rounded-md accent-container border border-border text-tertiary">
-        Free
+        6/24/25
       </div>
     </Checkbox>
-  );
-};
-
-const Subscriber = (props: {
-  profile: AppBskyActorProfile.Record;
-  did: string;
-  handle: string | null;
-}) => {
-  return (
-    <div className="subscriber">
-      <a
-        target="_blank"
-        href={`https://bsky.app/profile/${props.did}`}
-        className="flex text-primary p-2 gap-1"
-      >
-        <div className="flex flex-row gap-2">
-          {props.profile.avatar && (
-            <img
-              className="rounded-full w-8 h-8"
-              src={
-                props.profile?.avatar &&
-                blobRefToSrc(props.profile.avatar.ref, props.did)
-              }
-              alt={props.profile.displayName}
-            />
-          )}
-          <div className="flex flex-col gap-1">
-            <h3>{props.profile.displayName}</h3>
-            <p>@{props.handle}</p>
-          </div>
-        </div>
-      </a>
-      <hr className="border-border last:hidden" />
-    </div>
   );
 };
 
