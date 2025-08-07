ede4673d7ed8d51e809a5e5a9f971c21846a1d08
Jared Pereira <jared@awarm.space>
2025-08-07 12:13:17 -0400

    add selected state to hr block

--- components/Blocks/HorizontalRule.tsx
+++ components/Blocks/HorizontalRule.tsx
@@ -1,3 +1,15 @@
-export const HorizontalRule = () => {
-  return <hr className="my-2 w-full border-border-light" />;
+import { useUIState } from "src/useUIState";
+import { BlockProps } from "./Block";
+
+export const HorizontalRule = (props: BlockProps) => {
+  let isSelected = useUIState((s) =>
+    s.selectedBlocks.find((b) => b.value === props.value),
+  );
+  return (
+    <hr
+      className={`my-2 w-full border-border-light
+    ${isSelected ? "block-border-selected !outline-offset-[3px]" : ""}
+  `}
+    />
+  );
 };
