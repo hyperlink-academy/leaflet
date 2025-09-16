abc3264689a4e1980a9203694e672d56f3f8ba48
Jared Pereira <jared@awarm.space>
2025-09-16 15:33:15 +0900

    add page block generated files

---
+++ lexicons/api/types/pub/leaflet/blocks/page.ts
@@ -0,0 +1,26 @@
+/**
+ * GENERATED CODE - DO NOT MODIFY
+ */
+import { ValidationResult, BlobRef } from '@atproto/lexicon'
+import { CID } from 'multiformats/cid'
+import { validate as _validate } from '../../../../lexicons'
+import { $Typed, is$typed as _is$typed, OmitKey } from '../../../../util'
+
+const is$typed = _is$typed,
+  validate = _validate
+const id = 'pub.leaflet.blocks.page'
+
+export interface Main {
+  $type?: 'pub.leaflet.blocks.page'
+  id: string
+}
+
+const hashMain = 'main'
+
+export function isMain<V>(v: V) {
+  return is$typed(v, id, hashMain)
+}
+
+export function validateMain<V>(v: V) {
+  return validate<Main & V>(v, id, hashMain)
+}
