99859b5741ac5e43289c3a76e54ace32614937b8
celine <celine@hyperlink.academy>
2025-09-19 07:39:37 -0400

    fixed issue in theme on pub

--- components/ThemeManager/PubThemeSetter.tsx
+++ components/ThemeManager/PubThemeSetter.tsx
@@ -91,7 +91,7 @@ export const PubThemeSetter = () => {
           compact
           disabled={
             !(
-              showPageBackground !== !!record?.theme?.showPageBackground ||
+              showPageBackground === !!record?.theme?.showPageBackground ||
               changes ||
               !!image?.file ||
               record?.theme?.backgroundImage?.width !== image?.repeat
