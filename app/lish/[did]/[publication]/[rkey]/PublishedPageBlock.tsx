29977585fbb5afe81213b43fd182e0d0ce03f1f0
Jared Pereira <jared@awarm.space>
2025-10-23 18:40:45 -0400

    pass empty polldata array to preview components

--- app/lish/[did]/[publication]/[rkey]/PublishedPageBlock.tsx
+++ app/lish/[did]/[publication]/[rkey]/PublishedPageBlock.tsx
@@ -161,6 +161,7 @@ export function PagePreview(props: {
           />
         )}
         <PostContent
+          pollData={[]}
           pages={[]}
           did={props.did}
           blocks={props.blocks}
