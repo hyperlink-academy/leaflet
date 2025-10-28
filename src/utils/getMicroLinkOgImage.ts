95c952b4bbd25755fd124a651d66415aeb384048
Jared Pereira <jared@awarm.space>
2025-10-28 13:46:02 -0400

    set cache-control headers instead of cdn-cache-control

--- src/utils/getMicroLinkOgImage.ts
+++ src/utils/getMicroLinkOgImage.ts
@@ -20,11 +20,16 @@ export async function getMicroLinkOgImage(
     },
   );
   const clonedResponse = response.clone();
-  if (clonedResponse.status == 200)
+  if (clonedResponse.status == 200) {
     clonedResponse.headers.set(
       "CDN-Cache-Control",
       "s-maxage=600, stale-while-revalidate=3600",
     );
+    clonedResponse.headers.set(
+      "Cache-Control",
+      "s-maxage=600, stale-while-revalidate=3600",
+    );
+  }
 
   return clonedResponse;
 }
