07c18f454207969d641581a21e291fc0a61c11b2
Jared Pereira <jared@awarm.space>
2025-11-21 17:32:13 -0500

    add notifications for bsky post mentions

--- app/api/inngest/functions/index_post_mention.ts
+++ app/api/inngest/functions/index_post_mention.ts
@@ -3,6 +3,8 @@ import { inngest } from "../client";
 import { AtpAgent, AtUri } from "@atproto/api";
 import { Json } from "supabase/database.types";
 import { ids } from "lexicons/api/lexicons";
+import { Notification, pingIdentityToUpdateNotification } from "src/notifications";
+import { v7 } from "uuid";
 
 export const index_post_mention = inngest.createFunction(
   { id: "index_post_mention" },
@@ -37,6 +39,12 @@ export const index_post_mention = inngest.createFunction(
       return { message: `No post found for ${event.data.post_uri}` };
     }
 
+    const documentUri = AtUri.make(
+      pub.identity_did,
+      ids.PubLeafletDocument,
+      path[0],
+    ).toString();
+
     await step.run("index-bsky-post", async () => {
       await supabaseServerClient.from("bsky_posts").insert({
         uri: bsky_post.uri,
@@ -45,13 +53,26 @@ export const index_post_mention = inngest.createFunction(
       });
       await supabaseServerClient.from("document_mentions_in_bsky").insert({
         uri: bsky_post.uri,
-        document: AtUri.make(
-          pub.identity_did,
-          ids.PubLeafletDocument,
-          path[0],
-        ).toString(),
+        document: documentUri,
         link: event.data.document_link,
       });
+    });
+
+    await step.run("create-notification", async () => {
+      // Only create notification if the quote is from someone other than the author
+      if (bsky_post.author.did !== pub.identity_did) {
+        const notification: Notification = {
+          id: v7(),
+          recipient: pub.identity_did,
+          data: {
+            type: "quote",
+            bsky_post_uri: bsky_post.uri,
+            document_uri: documentUri,
+          },
+        };
+        await supabaseServerClient.from("notifications").insert(notification);
+        await pingIdentityToUpdateNotification(pub.identity_did);
+      }
     });
   },
 );
