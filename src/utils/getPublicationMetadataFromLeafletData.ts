ae408039bb2717bf7bc2cf6bda94eede85b17d24
Jared Pereira <jared@awarm.space>
2025-11-26 16:59:40 -0500

    fix prop access issue

--- src/utils/getPublicationMetadataFromLeafletData.ts
+++ src/utils/getPublicationMetadataFromLeafletData.ts
@@ -29,14 +29,14 @@ export function getPublicationMetadataFromLeafletData(
     | null =
     data?.leaflets_in_publications?.[0] ||
     data?.permission_token_rights[0].entity_sets?.permission_tokens?.find(
-      (p) => p.leaflets_in_publications.length,
+      (p) => p.leaflets_in_publications?.length,
     )?.leaflets_in_publications?.[0];
 
   // If not found, check for standalone documents
   let standaloneDoc =
     data?.leaflets_to_documents?.[0] ||
     data?.permission_token_rights[0].entity_sets?.permission_tokens.find(
-      (p) => p.leaflets_to_documents.length,
+      (p) => p.leaflets_to_documents?.length,
     )?.leaflets_to_documents?.[0];
   if (!pubData && standaloneDoc) {
     // Transform standalone document data to match the expected format
