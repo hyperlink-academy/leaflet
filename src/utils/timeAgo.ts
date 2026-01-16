a355cc99bcaddb81fba649f2257af8ccdd777601
celine <celine@hyperlink.academy>
2026-01-16 15:55:11 -0500

    add more resolution to timeAgo, show createdAt on comments

--- src/utils/timeAgo.ts
+++ src/utils/timeAgo.ts
@@ -6,10 +6,16 @@ export function timeAgo(timestamp: string): string {
   const diffMinutes = Math.floor(diffSeconds / 60);
   const diffHours = Math.floor(diffMinutes / 60);
   const diffDays = Math.floor(diffHours / 24);
+  const diffWeeks = Math.floor(diffDays / 7);
+  const diffMonths = Math.floor(diffDays / 30);
   const diffYears = Math.floor(diffDays / 365);
 
   if (diffYears > 0) {
     return `${diffYears} year${diffYears === 1 ? "" : "s"} ago`;
+  } else if (diffMonths > 0) {
+    return `${diffMonths} month${diffMonths === 1 ? "" : "s"} ago`;
+  } else if (diffWeeks > 0) {
+    return `${diffWeeks} week${diffWeeks === 1 ? "" : "s"} ago`;
   } else if (diffDays > 0) {
     return `${diffDays} day${diffDays === 1 ? "" : "s"} ago`;
   } else if (diffHours > 0) {
