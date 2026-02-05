acbb960485bc8649017b7f82c68a1433ac8ee248
celine <celine@hyperlink.academy>
2026-02-05 10:24:21 -0500

    oops forgot the new files

---
+++ app/(home-pages)/reader/GlobalContent.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const GlobalContent = () => {
+  return (
+    <div className="flex flex-col gap-2 container bg-[rgba(var(--bg-page),.7)] sm:p-4 p-3 justify-between text-center text-tertiary">
+      Nothing here yet…
+    </div>
+  );
+};
