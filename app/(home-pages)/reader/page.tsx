f3b953b0c33f556bd391f877d2ab60c11c812ccb
Jared Pereira <jared@awarm.space>
2026-02-12 15:49:21 +0400

    render what's hot for logged out users and don't render subs

--- app/(home-pages)/reader/page.tsx
+++ app/(home-pages)/reader/page.tsx
@@ -1,7 +1,16 @@
+import { getIdentityData } from "actions/getIdentityData";
 import { getReaderFeed } from "./getReaderFeed";
+import { getHotFeed } from "./getHotFeed";
 import { InboxContent } from "./InboxContent";
+import { GlobalContent } from "./GlobalContent";
 
 export default async function Reader() {
+  let identityData = await getIdentityData();
+  if (!identityData?.atp_did) {
+    const feedPromise = getHotFeed();
+    return <GlobalContent promise={feedPromise} />;
+  }
+
   const feedPromise = getReaderFeed();
   return <InboxContent promise={feedPromise} />;
 }
