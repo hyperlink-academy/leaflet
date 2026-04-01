14d5617b67abfaf91c336d0baa5e89455f88ecea
Jared Pereira <jared@awarm.space>
2026-04-01 15:11:52 -0700

    fix opening at mentions

--- components/Blocks/TextBlock/mountProsemirror.ts
+++ components/Blocks/TextBlock/mountProsemirror.ts
@@ -138,8 +138,12 @@ export function useMountProsemirror({
 
           // Check for atMention inline nodes
           if (node?.type === schema.nodes.atMention) {
-            const url = atUriToUrl(node.attrs.atURI);
-            window.open(url, "_blank", "noopener,noreferrer");
+            if (node.attrs.href)
+              window.open(node.attrs.href, "_blank", "noopener,noreferrer");
+            else {
+              const url = atUriToUrl(node.attrs.atURI);
+              window.open(url, "_blank", "noopener,noreferrer");
+            }
             return;
           }
           if (node.nodeSize - 2 <= _pos) return;
