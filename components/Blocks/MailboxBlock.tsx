9e90e8cc87d7669ade4e2fb948b2ef38bd4568b2
Jared Pereira <jared@awarm.space>
2026-04-29 11:10:15 -0400

    restore archive on mailbox blocks

--- components/Blocks/MailboxBlock.tsx
+++ components/Blocks/MailboxBlock.tsx
@@ -1,5 +1,7 @@
 import { BlockProps, BlockLayout } from "./Block";
 import { useUIState } from "src/useUIState";
+import { useEntity, useReplicache } from "src/replicache";
+import { focusPage } from "src/utils/focusPage";
 
 export const MailboxBlock = (
   props: BlockProps & {
@@ -10,6 +12,8 @@ export const MailboxBlock = (
   let isSelected = useUIState((s) =>
     s.selectedBlocks.find((b) => b.value === props.entityID),
   );
+  let archive = useEntity(props.entityID, "mailbox/archive");
+  let { rep } = useReplicache();
   return (
     <BlockLayout
       isSelected={!!isSelected}
@@ -24,6 +28,22 @@ export const MailboxBlock = (
       <div className="text-tertiary text-sm">
         Email subscriptions have moved to publication newsletters.
       </div>
+      {archive && (
+        <button
+          className="text-accent-contrast hover:underline text-sm mt-1"
+          onMouseDown={(e) => {
+            e.preventDefault();
+            if (rep) {
+              useUIState
+                .getState()
+                .openPage(props.parent, archive.data.value);
+              focusPage(archive.data.value, rep);
+            }
+          }}
+        >
+          See past posts
+        </button>
+      )}
     </BlockLayout>
   );
 };
