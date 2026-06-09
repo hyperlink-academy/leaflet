19a2a3d3893c7e89fcacc1178b6393b5bdfb3b43
celine <celine@hyperlink.academy>
2026-06-09 16:10:18 -0400

    little tweak

--- components/Subscribe/HandleSubscribe.tsx
+++ components/Subscribe/HandleSubscribe.tsx
@@ -81,7 +81,7 @@ export const SubscribeWithHandle = (props: {
   if (props.user.loggedIn && props.user.handle) {
     return (
       <div className="flex flex-col gap-2 w-full mx-auto min-w-0">
-        <div className="flex items-center gap-1 min-w-0">
+        <div className="flex items-center gap-1 min-w-0 w-fit mx-auto">
           <ButtonPrimary
             compact={props.compact}
             className={`mx-auto max-w-full grow shrink! min-w-0 ${props.compact && "text-sm"}`}
