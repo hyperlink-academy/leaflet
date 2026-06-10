cd7cfeedcde4773a7213400ea22f41798130c825
celine <celine@hyperlink.academy>
2026-06-10 16:49:14 -0400

    update some copy

--- app/(app)/lish/[did]/[publication]/dashboard/EditPagesNavLink.tsx
+++ app/(app)/lish/[did]/[publication]/dashboard/EditPagesNavLink.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { ActionButton } from "components/ActionBar/ActionButton";
-import { BlockDocPageSmall } from "components/Icons/BlockDocPageSmall";
+import { PaintSmall } from "components/Icons/PaintSmall";
 import { useRouter } from "next/navigation";
 import { useHasEntitlement } from "src/hooks/useEntitlement";
 
@@ -17,8 +17,8 @@ export function EditPagesNavLink(props: {
   return (
     <ActionButton
       id="edit-pages-button"
-      icon={<BlockDocPageSmall />}
-      label="Edit Pages"
+      icon={<PaintSmall />}
+      label="Customize"
       onClick={() =>
         // The editor creates the publication's draft leaflet on first visit.
         router.push(`/lish/${props.did}/${props.publicationName}/edit`)
