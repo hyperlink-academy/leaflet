72451afb0b22bd414add5489373dc5a88d4c883f
celine <celine@hyperlink.academy>
2026-06-22 16:43:55 -0400

    sale pricing update

--- app/(app)/lish/[did]/[publication]/UpgradeModal.tsx
+++ app/(app)/lish/[did]/[publication]/UpgradeModal.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { createCheckoutSession } from "actions/createCheckoutSession";
 import { DotLoader } from "components/utils/DotLoader";
 import { ToggleGroup } from "components/ToggleGroup";
+import { SaleSticker } from "components/SaleSticker";
 
 export const UpgradeContent = () => {
   let [cadence, setCadence] = useState<"year" | "month">("year");
@@ -27,46 +28,68 @@ export const UpgradeContent = () => {
     <div className="flex flex-col justify-center text-center sm:gap-4 gap-2 w-full">
       <h2>Get Leaflet Pro!</h2>
       <div className="flex sm:flex-row flex-col sm:gap-8 gap-4 items-stretch w-full">
-        <div className="text-secondary sm:py-6 py-4 px-4">
+        <div className="text-secondary sm:w-64 sm:py-6 py-4 px-4">
           <div className="font-bold text-primary">Analytics</div>
-          <div className="">Views per Post</div>
-          <div className="">Subscriber Counts</div>
-          <div className="">Top Referrers</div>
+          <div className="text-sm italic flex flex-col gap-0">
+            <div className="">
+              Views per Post, Subscriber Counts, Top Referrers
+            </div>
+          </div>
           <hr className="my-4 border-border-light" />
           <div className="font-bold text-primary">Emails</div>
           <div className="text-sm italic">
             First 1,000 subscribers included, $5/1K after that
           </div>
           <hr className="my-4 border-border-light" />
+
+          <div className="font-bold text-primary">Group Publications</div>
+
+          <hr className="my-4 border-border-light" />
           <div className="font-bold text-primary">Coming ASAP</div>
           <div className="">Paid Membership</div>
         </div>
         <div className="sm:w-64  w-full accent-container flex justify-center items-center">
-          <div className="flex flex-col justify-center text-center py-6">
-            <ToggleGroup
-              className="mb-3"
-              value={cadence}
-              onChange={setCadence}
-              options={[
-                { value: "year", label: "Yearly" },
-                { value: "month", label: "Monthly" },
-              ]}
-            />
-            <div className="flex gap-1 items-baseline justify-center">
+          <div className="flex flex-col justify-center text-center py-6 px-3 ">
+            <div className="mx-auto mb-3">
+              <ToggleGroup
+                value={cadence}
+                onChange={setCadence}
+                options={[
+                  { value: "year", label: "Yearly" },
+                  { value: "month", label: "Monthly" },
+                ]}
+              />
+            </div>
+
+            <div className="flex gap-1 items-baseline justify-center pb-4">
               <div className="text-2xl font-bold leading-tight">
                 {cadence === "year" ? (
-                  <div className="flex gap-1">
-                    <div className="relative">$120</div>
+                  <div className="flex gap-1 items-baseline">
+                    <div className="relative text-tertiary text-lg ">
+                      $120
+                      <div className="-rotate-16 absolute h-0.5 top-3 -left-1 -right-1 bg-tertiary border-1 " />
+                    </div>
                     <div>$90</div>
                   </div>
                 ) : (
                   "$12"
                 )}
               </div>
-              <div className="text-secondary pb-4">
+              <div className="text-secondary">
                 {cadence === "year" ? "/year" : "/month"}
               </div>
             </div>
+            {cadence === "year" && (
+              <div className="frosted-container pt-2 pb-4 px-3  text-secondary gap-2 text-center leading-snug   mb-4 w-full ">
+                <div className="font-bold flex items-center gap-1 justify-center w-full">
+                  <SaleSticker width={48} />
+                  your first year!{" "}
+                </div>
+                <div className="text-sm font-normal!">
+                  Get your Summer Pass, <br /> only available until July 31
+                </div>
+              </div>
+            )}
             <ButtonPrimary
               fullWidth
               className="mx-auto"
@@ -75,6 +98,7 @@ export const UpgradeContent = () => {
             >
               {loading ? <DotLoader /> : "Get it!"}
             </ButtonPrimary>
+
             {error && <div className="text-sm text-red-500 mt-2">{error}</div>}
           </div>
         </div>
