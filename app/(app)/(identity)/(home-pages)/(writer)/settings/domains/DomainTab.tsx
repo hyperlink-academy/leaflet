b00978c10303b44b80ddaad5ae4e75e1deb32a20
celine <celine@hyperlink.academy>
2026-08-10 12:32:15 -0400

    fixing bugs in domain settings

--- app/(app)/(identity)/(home-pages)/(writer)/settings/domains/DomainTab.tsx
+++ app/(app)/(identity)/(home-pages)/(writer)/settings/domains/DomainTab.tsx
@@ -56,30 +56,29 @@ export function DomainTab(props: {
           </div>
         </SettingsSection>
       )}
-      {unassignedDomains.length > 0 && (
-        <SettingsSection
-          title="Unassigned Domains"
-          action={
-            <Modal
-              asChild
-              trigger={
-                <ButtonPrimary compact className="">
-                  <AddTiny /> Add Domain
-                </ButtonPrimary>
-              }
-            >
-              <AddDomainForm />
-            </Modal>
-          }
-        >
-          <p>Assign domains in the leaflet settings or publication settings.</p>
-          <div className="flex flex-col gap-2">
-            {unassignedDomains.map((domain) => (
-              <UnassignedDomain key={domain.domain} domain={domain} />
-            ))}
-          </div>
-        </SettingsSection>
-      )}
+
+      <SettingsSection
+        title="Unassigned Domains"
+        action={
+          <Modal
+            asChild
+            trigger={
+              <ButtonPrimary compact className="">
+                <AddTiny /> Add Domain
+              </ButtonPrimary>
+            }
+          >
+            <AddDomainForm />
+          </Modal>
+        }
+      >
+        <p>Assign domains in the leaflet settings or publication settings.</p>
+        <div className="flex flex-col gap-2">
+          {unassignedDomains.map((domain) => (
+            <UnassignedDomain key={domain.domain} domain={domain} />
+          ))}
+        </div>
+      </SettingsSection>
     </>
   );
 }
